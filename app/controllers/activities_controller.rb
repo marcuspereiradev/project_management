ce631cd8d9c2986aab26db79d871376e89ba3750
Marcus Pereira <marcus.work@hotmail.com>
2021-05-07 11:17:45 -0300

    Adding edit activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,10 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:edit, :update]
+
+  def edit
+    # debugger
+  end
+
   def create
     @project = Project.find(params[:project_id])
     @activity = @project.activities.new(activity_params)
@@ -11,9 +17,19 @@ class ActivitiesController < ApplicationController
     end
   end
 
+  def update
+      if @activity.update(activity_params)
+        flash[:success] = "Activity was successfully updated"
+        redirect_to @activity.project
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
   def destroy
     @project = Project.find(params[:id])
-    @activity = Activity.find(params[:project_id])
+    @activity = @project.activities.find(params[:project_id])
     if @activity.destroy
       flash[:success] = 'Activity was successfully deleted.'
       redirect_to @project
@@ -26,7 +42,7 @@ class ActivitiesController < ApplicationController
 
   def finished
     @project = Project.find(params[:id])
-    @activity = Activity.find(params[:project_id])
+    @activity = @project.activities.find(params[:project_id])
     @activity.finished = !@activity.finished
     @activity.save
     redirect_to @project
@@ -36,5 +52,9 @@ class ActivitiesController < ApplicationController
 
   def activity_params
     params.require(:activity).permit(:activity_name, :start_date, :end_date)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
   end
 end
